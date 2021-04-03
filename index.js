2204226230b132739f06f174d33bf02f1bca8f26
Fazaa Majdeddine <79419308+fazaamajdeddine@users.noreply.github.com>
2021-04-03 23:40:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$("h1").addClass("big-title");
+$("button").css("backgroundColor" , "yellow");
+$("h1").text("bye");
+$("button").text("im a button :)");
+$("button").html("<em>yeahhh</em>");
+
+$("button").click(function(){
+    $(this).hide();
+})
+
+
+
+
